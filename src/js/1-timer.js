7d61a617d072be607462446bd043f380177cf9d0
Viktoriia Popel <xructa2009@gmail.com>
2025-08-05 15:19:17 +0200

    fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,22 +1,26 @@
 import flatpickr from "flatpickr";
 
-// Імпорт стилів (щоб вигляд календаря був нормальний)
+
 import "flatpickr/dist/flatpickr.min.css";
 
 let userSelectedDate = null;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
 
 
 flatpickr("#datetime-picker", {
   enableTime: true,
   dateFormat: "Y-m-d H:i",
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        const today = new Date();
+        const selectedDate = selectedDates[0];
+        today.setHours(0, 0, 0, 0);
+        if (selectedDate < today) {
+            alert(`"Please choose a date in the future"`);
+        }
+        else {
+            userSelectedDate = selectedDate;
+        }
+  },
 });
